5a9eb9a8d748a21c023c485280b937a37f2e9d9f
Rishabh Mittal <rismitta@publicisgroupe.net>
2021-03-09 12:57:50 +0530

    project checkin

---
+++ src/com/dsa/array/first.java
@@ -0,0 +1,4 @@
+package com.dsa.array;
+
+public class first {
+}
---
+++ src/com/dsa/tree/TreeNode.java
@@ -0,0 +1,11 @@
+package com.dsa.tree;
+
+public class TreeNode {
+    private TreeNode left;
+    private TreeNode right;
+    private int val;
+
+    public TreeNode(int val) {
+        this.val = val;
+    }
+}
